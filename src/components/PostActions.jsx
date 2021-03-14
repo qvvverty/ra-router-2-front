e7937e7f5d9d2624c975df23dd18c28821564d30
Leonid Kostyuchenko <kstchnko@gmail.com>
2021-03-14 13:16:12 +0300

    add small fallbacks

--- src/components/PostActions.jsx
+++ src/components/PostActions.jsx
@@ -2,12 +2,24 @@ import { useState, useContext } from 'react';
 import PostContext from '../contexts/PostsContext';
 import NewPost from './PostEditForm';
 import Post from './Post';
+import { Link } from 'react-router-dom';
 
 export default function PostActions(props) {
   const [isEditing, setEditing] = useState(false);
 
   const { posts } = useContext(PostContext);
   const post = posts.find(post => post.id === +props.match.params.id);
+
+  if (!post) {
+    return (
+      <>
+        <Post />
+        <div className="edit-delete-container">
+          <Link to="/" className="link"><div className="post-action-btn btn-edit single-btn">Back</div></Link>
+        </div>
+      </>
+    )
+  }
 
   const editBtnHandler = () => {
     setEditing(!isEditing);
@@ -27,10 +39,13 @@ export default function PostActions(props) {
     <div className="post-actions-wrapper">
       <Post {...post} />
       {isEditing ?
-      <NewPost {...post} {...props} />
-      : <div className="post-actions-container">
-          <div className="post-action-btn btn-edit" onClick={editBtnHandler}>Edit</div>
-          <div className="post-action-btn btn-delete" onClick={deleteBtnHandler}>Delete</div>
+        <NewPost {...post} {...props} />
+        : <div className="post-actions-container">
+          <Link to="/" className="link"><div className="post-action-btn btn-edit">Back</div></Link>
+          <div className="edit-delete-container">
+            <div className="post-action-btn btn-edit" onClick={editBtnHandler}>Edit</div>
+            <div className="post-action-btn btn-delete" onClick={deleteBtnHandler}>Delete</div>
+          </div>
         </div>}
     </div>
   )
